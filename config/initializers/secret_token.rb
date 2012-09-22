d65b7495e9232dc48caec451642a827c631a78f5
Alexander Rao <Alexanderrao@gmail.com>
2012-09-22 04:10:55 -0400

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickupsports::Application.config.secret_token = '565537a922f3bc6d8310285cf2b13ad9d108f5b2aaa009fecccaa5c27240289dfbf026f59c210a0432c8591c5ac19cec654fde6fc6320da46f449fa55933f820'
